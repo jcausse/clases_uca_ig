5c7967b6c6ad5b07630c8a86b45b50dce1340e56
Juan Ignacio Causse <jcausse@itba.edu.ar>
2025-01-31 20:58:48 -0300

    Nuevo archivo de ejercicio (falta hacer)

---
+++ guia_5/_ej4.py
@@ -0,0 +1,13 @@
+"""
+Ejercicio 4:
+
+Desarrollar una función booleana "principioFin" que reciba como parámetro un texto y                       
+detecte si la primera palabra del texto es exactamente igual a la última.  
+Por Ejemplo:  
+    “Barón!, urgente ya ha nacido! Es un varón!”        → False  
+    “Entre que ya se vienen los hombres de enfrente”    → False  
+    “Hombre de poca fe, he dicho! Vamos, vamos hombre!” → True 
+Desde el programa principal ingresar por teclado un texto, invocar a la función y mostrar                             
+por pantalla un mensaje que indique si cumple o no con la condición. Ejemplo:
+"""
+
