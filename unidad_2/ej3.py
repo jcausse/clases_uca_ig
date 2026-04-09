77bd54b1fb89d54df20892902993af03a11dc18e
Juan I. Causse <jcausse@itba.edu.ar>
2026-04-09 19:35:27 -0300

    Updates

--- unidad_2/ej3.py
+++ unidad_2/ej3.py
@@ -4,16 +4,18 @@ Ejercicio 3:
 El concepto de paridad es de fundamental importancia para la detección y corrección de                           
 errores en el almacenamiento y transmisión de datos en los sistemas informáticos. Para                         
 cada dato en binario se genera un bit adicional (bit de paridad) de manera tal que este                                 
-será 1 (​uno​) si la suma de la cantidad de 1 (​unos​) presentes en el dato es impar y 0                                       
-(​cero​) si esta suma es par.  
+será 1 (uno) si la suma de la cantidad de 1 (unos) presentes en el dato es impar y 0                                       
+(cero) si esta suma es par.  
  
-Programar la función booleana ​paridad ​que recibe como parámetro un número binario                       
+Programar la función booleana paridad que recibe como parámetro un número binario                       
 de hasta 8 bits y genera y retorna el bit de paridad correspondiente. Utilizarla en un                               
 programa que solicite al usuario el ingreso del número binario, invoque a la función e                             
 informe la paridad generada.
 """
 
 """
+IDEA:
+
 Un byte, que puede ser por ejemplo 1101 1001 va a tener un:
 * Most Significative Bit (MSb) => el de mas a la izquierda
 * Less Significative Bit (LSb) => el de mas a la derecha
@@ -33,9 +35,22 @@ print(get_bit(byte, 4)) # Imprime 1
 print(get_bit(byte, 1)) # Imprime 0
 """
 
+# Dado un byte y un numero de bit, esta funcion usa los conceptos vistos para extraer el
+# n-esimo digito de un numero, justamente para extraer un bit determinado. Ahora bien,
+# por que se usa base 10 y no base 2 si es un binario?
+# Porque las variables numericas enteras en Python almacenan los datos en base 10. Entonces
+# si declaramos x = 1101 (intentando almacenar un binario), en realidad el numero almacenado
+# es mil ciento uno (base 10). Lo que hacemos es usar base 10 para "simular" un binario usando
+# solo ceros y unos.
 def get_bit(byte, bit_num):
     return (byte // (10 ** bit_num)) % 10
 
+# Obtenemos cada bit del byte, y, en una variable ones_count sumamos todos los bits
+# Como los bits solo valen 0 o 1, la suma total de todos los bits del byte equivale a la
+# cantidad total de unos presentes en el byte.
+# Luego, la funcion retorna dicha suma % 2:
+# - Si ones_count es par (hay una cantidad par de unos en el byte) retorna 0 (todo par % 2 da 0)
+# - Si ones_count es impar (hay una cantidad impar de unos en el byte) retorna 1 (todo impar % 2 da 1)
 def parity(byte):
     ones_count  = get_bit(byte, 0)
     ones_count += get_bit(byte, 1)
--- unidad_2/ej6.py
+++ unidad_2/ej6.py
@@ -7,10 +7,26 @@ los ítems de una cena: vestimenta, plato y bebida. Luego el programa muestra po
 pantalla la cena que resulta de elegir cada ítem al azar
 """
 
-from random import randint
+"""
+IDEA:
+
+La idea principal de este ejercicio es que al multiplicar un string por 1, el mismo queda igual:
+- 'hola' * 1 resulta 'hola'
+pero, al multiplicar un string por cero, resulta el string vacio:
+- 'chau' * 0 resulta ''
+
+Uno puede "elegir al azar" uno de los dos strings obteniendo un numero aleatorio x que sea 0 o 1, y
+luego multiplicando x por uno de los strings, y la expresion (1 - x) por el otro.
+- Cuando x = 1:
+  'hola' * x + 'chau' * (1 - x) ==> 'hola' * 1 + 'chau' * 0 ==> 'hola' + '' ==> 'hola'
+- Cuando x = 0:
+  'hola' * x + 'chau' * (1 - x) ==> 'hola' * 0 + 'chau' * 1 ==> '' + 'chau' ==> 'chau'
+"""
+
+import random
 
 def zero_or_one():
-    return randint(0, 1)
+    return random.randint(0, 1)
 
 def main():
     vestimenta_1    = input('Ingrese la primera opcion de vestimenta: ')
@@ -20,7 +36,9 @@ def main():
     bebida_1        = input('Ingrese la primera opcion de bebida: ')
     bebida_2        = input('Ingrese la segunda opcion de bebida: ')
 
+    # Obtener un numero aleatorio entre 0 y 1
     random_num = zero_or_one()
+    # Seleccionar una opcion haciendo multiplicacion y concatenacion de strings
     vestimenta = (1 - random_num) * vestimenta_1 + random_num * vestimenta_2
 
     random_num = zero_or_one()
