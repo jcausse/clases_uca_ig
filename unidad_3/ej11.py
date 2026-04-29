8135518b7bbe9277cf6489f33a25ed5e01f953fa
Juan I. Causse <jcausse@itba.edu.ar>
2026-04-29 12:07:59 -0300

    Updates

--- unidad_3/ej11.py
+++ unidad_3/ej11.py
@@ -1,28 +1,48 @@
 def minimo(a, b):
-    if a < b:
+    if a <= b:
         ret = a
     else:
         ret = b
     return ret
 
-def mensaje(cant_1m, cant_5m, tendido):
-    # Caños 5m
-    necesarios_5m = tendido // 5                # Veo cuantos caños de 5 necesito
-    usados_5m = minimo(cant_5m, necesarios_5m)  # Y uso el minimo entre los que necesito y los que tengo
-    cubierto = 5 * usados_5m                    # Tendido cubierto hasta ahora (con caños de 5m)
+def mensaje(cant_1, cant_5, total):
+    cant_ideal_5 = total // 5   # Cantidad ideales: Cuantos usaria de cada longitud si tuviera infinitos de cada una
+    
+    # La cantidad de caños de 5 que voy a usar es el minimo entre los que necesitaria de manera ideal (suponiendo que tengo
+    # infinitos) y los que verdaderamente tengo. Nunca voy a usar mas de los que idealmente necesitaria ni mas de los que
+    # realmente tengo.
+    usados_5 = minimo(cant_ideal_5, cant_5)
+    
+    # Lo que me sobra a cubrir con caños de 1 es el total menos lo que ya cubri con la cantidad de caños de 5 que use
+    # Si cant_ideal_5 <= cant_5 (necesitaria menos o las misma cantidad que tengo), entonces el restante va a ser de 0 a 4 metros
+    # En cambio, si cant_ideal_5 > cant_5 (necesitaria idealmente mas de los que tengo), entonces uso todos los que tengo pero me
+    # va a faltar cubrir un tendido mayor o igual a 5 metros, que lo debo cubrir enteramente con caños de 1 metro. Ejemplos:
+    # - cant_5 = 3, total = 21, uso los 3 que tengo, y necesito cubrir 6 metros mas solo con caños de 1 (los de 5 se acabaron)
+    # - cant_5 = 3, total = 20, uso los 3 que tengo, y necesito cubrir 5 metros mas solo con caños de 1 (los de 5 se acabaron)
+    restante = total - 5 * usados_5
+    
+    # Anteriormente quedo determinado un restante a cubrir con caños de 1. La cantidad de caños de 1 que necesito para cubrir ese
+    # tramo que falta es (restante // 1) (que es igual a restante). Debo tener esa cantidad de caños de 1.
+    # Ahora bien, puedo tener los suficientes caños de 1 como para cubrir el restante o no. Si los tengo, se puede cubrir. Si no
+    # los tengo no puedo cubrir el total solicitado.
+    if restante > cant_1:       # Si necesito mas caños de 1 que los que tengo para cubrir lo que falta
+        ret = 'No es posible cubrir el tendido.'
+    else:                       # Si me alcanzan
+        ret =   'Es posible cubrir el tendido.\n' + \
+                'Sugerencia:\n' + \
+                '  {} unidades de caño de 5 metros\n' + \
+                '  {} unidades de caño de 1 metro'
 
-    # Caños 1m
-    necesarios_1m = tendido - cubierto          # Necesito tantos caños de 1m como metros me falten cubrir
-    if necesarios_1m > cant_1m:                 # En este caso, si necesitara mas de los que tengo, no se puede cubrir el tendido
-        ret = "No es posible cubrir el tendido."
-    else:                                       # Si tengo justo los que necesito o mas, si se puede
-        ret = "Es posible cubrir el tendido.\nSugerencia:\n{} unidades de caño de 5 metros.\n{} unidades de caño de 1 metro.".format(usados_5m, necesarios_1m)
+        # Recordar que los str son inmutables! Para formatear y "cambiar" el str hay que asignar de nuevo
+        ret = ret.format(usados_5, restante) 
+    
     return ret
 
 def main():
-    cant_1m = int(input('Cantidad de caños de 1 metro:  '))
-    cant_5m = int(input('Cantidad de caños de 5 metros: '))
-    tendido = int(input('Metros totales a cubrir:       '))
-    print(mensaje(cant_1m, cant_5m, tendido))
-
+    cant_1 = int(input('Cantidad de caños de 1 metros: '))
+    cant_5 = int(input('Cantidad de caños de 5 metros: '))
+    total  = int(input('Metros totales a cubrir: '))
+    msg = mensaje(cant_1, cant_5, total)
+    print(msg)
+    
 main()
